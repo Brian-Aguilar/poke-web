751a4ae0ef14f67a6f9b8b01103f9f6e9e07f2e1
Brian Aguilar D <aguilar.16999@gmail.com>
2021-07-02 11:59:28 -0500

    Agregando el PokemonAppProviders, reactContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import GlobalStyle from "./components/GlobalStyle";
+import { PokemonAppProvider } from "./R";
 import AppRouter from "./routes";
 
 const App = () => {
   return (
-    <>
+    <PokemonAppProvider>
       <GlobalStyle />
       <AppRouter />
-    </>
+    </PokemonAppProvider>
   );
 };
 export default App;
